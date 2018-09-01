02dbf92d9bf8d9e0b1f45300f044ed7d9d5c8ec7
Ville Jänkälä <ville.jankala@gmail.com>
2018-09-01 15:11:42 +0300

    Build polygons

--- src/index.js
+++ src/index.js
@@ -22,9 +22,40 @@ class LevelEditorGUI {
     });
   }
   addEventListeners() {
+    document.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+    });
     this._canvas.addEventListener("mousedown", (e) => {
-      let pol = this._editor.level.polygons[0];
-      this._editor.createVertex(this.xtovx(e.clientX), this.ytovy(e.clientY), pol);
+      switch (e.button) {
+        case 0:
+          if (!this._ap) {
+            let polygonId = this._editor.createPolygon([{
+              x: this.xtovx(e.clientX),
+              y: this.ytovy(e.clientY)
+            }], false);
+
+            let vertexId = this._editor.createVertex(this.xtovx(e.clientX), this.ytovy(e.clientY), this._editor.findPolygon(polygonId));
+            this._ap = this._editor.findPolygon(polygonId);
+            this._av = this._editor.findVertex(vertexId, this._ap);
+          } else {
+            let vertexId = this._editor.createVertex(this.xtovx(e.clientX), this.ytovy(e.clientY), this._ap, this._av.id);
+            this._av = this._editor.findVertex(vertexId, this._ap);
+          }
+          break;
+        case 2:
+          if (this._ap && this._av) {
+            this._editor.deleteVertex(this._ap, this._av.id);
+            this._av = null;
+            this._ap = null;
+          }
+      }
+    });
+
+    this._canvas.addEventListener("mousemove", (e) => {
+      if (this._av) {
+        this._av.x = this.xtovx(e.clientX);
+        this._av.y = this.ytovy(e.clientY);
+      }
     });
   }
   xtovx(x) {
@@ -71,11 +102,17 @@ class LevelEditorGUI {
     });
     return i % 2 !== 0;
   }
+
   isClockwise(vertices) {
-    var sum = 0
-    for (var i = 0; i < vertices.length - 1; i++) {
-      var cur = vertices[i],
-        next = vertices[i + 1]
+    let sum = 0;
+    let v = vertices.slice(0);
+    v.push({
+      x: vertices[0].x,
+      y: vertices[0].y
+    });
+    for (let i = 0; i < v.length - 1; i++) {
+      let cur = v[i],
+        next = v[i + 1];
       sum += (next.x - cur.x) * (next.y + cur.y)
     }
     return sum > 0
@@ -83,6 +120,7 @@ class LevelEditorGUI {
   render() {
     this._ctx.clearRect(0, 0, this._canvas.width, this._canvas.height);
     this._ctx.fillStyle = "#95a0a8";
+    this._ctx.strokeStyle = "red";
     this._ctx.save();
     this._ctx.translate(this._viewPortOffset.x, this._viewPortOffset.y);
     this._ctx.scale(this._zoom, this._zoom);
@@ -98,12 +136,13 @@ class LevelEditorGUI {
         else
           this._ctx.lineTo(v.x, v.y);
       });
-      this._ctx.lineTo(p.vertices[0].x, p.vertices[0].y);
+      this._ctx.lineTo(d[0].x, d[0].y);
     });
     this._ctx.closePath();
     this._ctx.clip();
     this._ctx.fillRect(this._viewPortOffset.x * -1, this._viewPortOffset.y * -1, this._canvas.width, this._canvas.height);
     this._ctx.restore();
+    this._ctx.stroke();
   }
 }
 
@@ -112,3 +151,21 @@ module.exports = LevelEditorGUI;
 let gui = new LevelEditorGUI();
 gui.init();
 gui.newLevel();
+
+gui._editor.createPolygon([{
+    x: 1,
+    y: 1
+  },
+  {
+    x: 1,
+    y: 2
+  },
+  {
+    x: 2,
+    y: 2
+  },
+  {
+    x: 2,
+    y: 1
+  }
+], false);
