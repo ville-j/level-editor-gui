bd0de402c09fa6e8488dc95959c5da9763e1fef8
Ville Jänkälä <ville.jankala@gmail.com>
2018-09-01 13:11:26 +0300

    Fill polygons

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-const Editor = require("level-editor");
+const LevelEditor = require("level-editor");
 
-class EditorGUI {
+class LevelEditorGUI {
   init() {
     this._viewPortOffset = {
       x: 50,
       y: 50
     }
     this._zoom = 50;
-    this._editor = new Editor();
+    this._editor = new LevelEditor();
     this._container = document.getElementById("level-editor-gui");
     this._canvas = document.createElement("canvas");
     this._ctx = this._canvas.getContext("2d", {
@@ -41,39 +41,74 @@ class EditorGUI {
     this._editor.newLevel();
   }
   loop() {
-    this.render();
     window.requestAnimationFrame(() => {
       this.loop();
     });
+    this.render();
+  }
+  pointInPolygon(x, y, vertices) {
+    let inside = false;
+    for (var i = 0, j = vertices.length - 1; i < vertices.length; j = i++) {
+      var xi = vertices[i].x,
+        yi = vertices[i].y;
+      var xj = vertices[j].x,
+        yj = vertices[j].y;
+
+      var intersect = ((yi > y) != (yj > y)) &&
+        (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+      if (intersect) inside = !inside;
+    }
+
+    return inside;
+  }
+  isGround(p) {
+    let i = 0;
+    this._editor.level.polygons.map(pp => {
+      if (pp.id !== p.id) {
+        if (this.pointInPolygon(p.vertices[0].x, p.vertices[0].y, pp.vertices))
+          i++;
+      }
+    });
+    return i % 2 !== 0;
+  }
+  isClockwise(vertices) {
+    var sum = 0
+    for (var i = 0; i < vertices.length - 1; i++) {
+      var cur = vertices[i],
+        next = vertices[i + 1]
+      sum += (next.x - cur.x) * (next.y + cur.y)
+    }
+    return sum > 0
   }
   render() {
     this._ctx.clearRect(0, 0, this._canvas.width, this._canvas.height);
-    this._ctx.strokeStyle = "#b349ff";
-    this._ctx.lineWidth = .8;
+    this._ctx.fillStyle = "#95a0a8";
     this._ctx.save();
     this._ctx.translate(this._viewPortOffset.x, this._viewPortOffset.y);
     this._ctx.scale(this._zoom, this._zoom);
-
+    this._ctx.beginPath();
     this._editor.level.polygons.map(p => {
+      let d = p.vertices.slice(0);
+      if (this.isGround(p) !== this.isClockwise(p.vertices))
+        d.reverse();
 
-      this._ctx.beginPath();
-
-      p.vertices.map((v, i) => {
+      d.map((v, i) => {
         if (i === 0)
           this._ctx.moveTo(v.x, v.y);
         else
           this._ctx.lineTo(v.x, v.y);
       });
-
-      this._ctx.closePath();
+      this._ctx.lineTo(p.vertices[0].x, p.vertices[0].y);
     });
+    this._ctx.closePath();
+    this._ctx.clip();
+    this._ctx.fillRect(this._viewPortOffset.x * -1, this._viewPortOffset.y * -1, this._canvas.width, this._canvas.height);
     this._ctx.restore();
-    this._ctx.stroke();
   }
 }
 
-module.exports = EditorGUI;
+module.exports = LevelEditorGUI;
 
-let gui = new EditorGUI();
+let gui = new LevelEditorGUI();
 gui.init();
 gui.newLevel();
